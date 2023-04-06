4dabc552e0457d93b7e1eb465e0b4a052a73cd5c
Colin <bestxt@qq.com>
2023-04-06 12:56:42 +0000

    修改sidebar-title

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -118,9 +118,9 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>AI Lab</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            设置——输入OpenAIKey 即可使用 By Colin.
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
